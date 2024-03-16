d6ca019f3d47cc309fc4d4da1f3279930a6cfa22
AlissonSP1511 <alisson.silva.pereira@gmail.com>
2024-03-15 21:20:20 -0300

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const routes = require('./routes/routes');
+app.use('/', routes);
+
+
+app.listen(3001, () => {
+    console.log("Servidor iniciado!");
+})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
